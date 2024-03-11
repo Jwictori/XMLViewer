de7d57b0bfb9d5c08e2a2bd483d4e665961662da
Jwictori <162752915+Jwictori@users.noreply.github.com>
2024-03-11 21:19:12 +0000

    Change of minWidth calculation when resizing

--- js/Resizer.js
+++ js/Resizer.js
@@ -1,33 +1,40 @@
 export function initializeResizer() {
-    const resizer = document.getElementById('resizer');
-    const leftPanel = document.getElementById('treeViewer');
-    const container = document.getElementById('container');
-    let isResizing = false;
-    let initialPosX = 0;
-    let initialWidth = 0;
-    let minWidth = 100;
-    
+  const resizer = document.getElementById("resizer");
+  const leftPanel = document.getElementById("treeViewer");
+  const container = document.getElementById("container");
+  let isResizing = false;
+  let initialPosX = 0;
+  let initialWidth = 0;
+  let minWidth = 100;
 
-    resizer.addEventListener('mousedown', function(e) {
-        e.preventDefault();
-        isResizing = true;
-        initialPosX = e.clientX;
-        initialWidth = leftPanel.offsetWidth;
+  resizer.addEventListener("mousedown", function (e) {
+    e.preventDefault();
+    isResizing = true;
+    initialPosX = e.clientX;
+    initialWidth = leftPanel.offsetWidth;
 
-        document.addEventListener('mousemove', handleMouseMove);
-        document.addEventListener('mouseup', function() {
-            isResizing = false;
-            document.removeEventListener('mousemove', handleMouseMove);
-            console.log("Mouse released, resizing stopped");
-        });
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", function () {
+      isResizing = false;
+      document.removeEventListener("mousemove", handleMouseMove);
+      console.log("Mouse released, resizing stopped");
     });
+  });
 
-    function handleMouseMove(e) {
-        if (!isResizing) return;
-        // Calculate the new basis
-        const deltaX = e.clientX - initialPosX;
-        const newFlexBasis = Math.max(initialWidth + deltaX, minWidth); // Apply a minimum width directly here
-        console.log(`New flex-basis: ${newFlexBasis}px`);
-        leftPanel.style.flexBasis = `${newFlexBasis}px`;
+  function handleMouseMove(e) {
+    if (!isResizing) return;
+    // Calculate the new basis
+    const deltaX = e.clientX - initialPosX;
+    let newFlexBasis = initialWidth + deltaX;
+    let setFlexBasis = 0;
+    if (newFlexBasis <= minWidth) {
+      setFlexBasis = minWidth;
+    } else if (newFlexBasis >= container.offsetWidth - minWidth) {
+      setFlexBasis = container.offsetWidth - minWidth;
+    } else {
+      setFlexBasis = initialWidth + deltaX;
     }
+    //console.log(`New flex-basis: ${setFlexBasis}px`);
+    leftPanel.style.flexBasis = `${setFlexBasis}px`;
+  }
 }
