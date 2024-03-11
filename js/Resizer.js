e842aaf8ae43c08b748cdb545c11f404c3fc703f
Jwictori <162752915+Jwictori@users.noreply.github.com>
2024-03-11 21:50:50 +0000

    Added debounce to resizing

--- js/Resizer.js
+++ js/Resizer.js
@@ -12,31 +12,39 @@ export function initializeResizer() {
     e.preventDefault();
     isResizing = true;
     initialPosX = e.clientX;
-    initialWidth = leftPanel.offsetWidth;
+    initialWidth = leftPanel.offsetWidth; // Capture the initial width
 
-    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mousemove", debouncedHandleMouseMove);
     document.addEventListener("mouseup", function () {
       isResizing = false;
-      document.removeEventListener("mousemove", handleMouseMove);
-      //console.log("Mouse released, resizing stopped");
+      document.removeEventListener("mousemove", debouncedHandleMouseMove);
+      //console.log("Mouse released, resizing stopped"); // For debug
     });
   });
 
-  function handleMouseMove(e) {
+  function debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
+
+  const debouncedHandleMouseMove = debounce(function(e) {
     if (!isResizing) return;
     // Calculate the new basis
     const deltaX = e.clientX - initialPosX;
-    let newFlexBasis = initialWidth + deltaX;
-    let setFlexBasis = 0;
-    if (newFlexBasis <= minWidth) {
-      setFlexBasis = minWidth;
-    } else if (newFlexBasis >= container.offsetWidth - minWidth) {
-      setFlexBasis = container.offsetWidth - minWidth;
-    } else {
-      setFlexBasis = initialWidth + deltaX;
-    }
-    //console.log(`New flex-basis: ${setFlexBasis}px`);
-    leftPanel.style.flexBasis = `${setFlexBasis}px`;
-    rightPanel.style.flexBasis = `${container.offsetWidth - setFlexBasis}px`;
-  }
+    let newFlexBasis = Math.max(minWidth, Math.min(container.offsetWidth - minWidth, initialWidth + deltaX));
+    // Directly apply the calculated flex-basis to the left panel
+    leftPanel.style.flexBasis = `${newFlexBasis}px`;
+    // Adjust the right panel's flex-basis based on the left panel's new size
+    rightPanel.style.flexBasis = `${container.offsetWidth - newFlexBasis}px`;
+    //console.log(`New flex-basis: ${setFlexBasis}px`); // For debug
+  }, 50); // 50 milliseconds delay
+
 }
