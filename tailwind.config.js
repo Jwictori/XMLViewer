6cf08f830dc7f079e8027f1de682f00ee8b6a86c
Jwictori <162752915+Jwictori@users.noreply.github.com>
2024-03-14 13:23:39 +0000

    Test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}", "./**/*.{html,js}"],
+  content: ['./index.html',
+			      './js/**/*.{html,js}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
 }
